f098e6e5b6bc5c1368f49a3510bfb3cc5576489d
Charlie Ding <charlie.ding@arup.com>
2020-08-28 12:10:26 +0100

    cors middleware added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const apiRouter = require("./routes/api.router.js");
 const { invalidPaths, Error400, Error404, Error500} = require("./errors/errors");
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/api", apiRouter);
 
